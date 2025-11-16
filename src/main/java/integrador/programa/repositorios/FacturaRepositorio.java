c5b446b3e554a0dc55febe53293f4f96d6bfea96
Nathelin <menachonatalian@gmail.com>
2025-11-16 12:11:38 -0300

    Métodos básicos para buscar Facturas.

--- src/main/java/integrador/programa/repositorios/FacturaRepositorio.java
+++ src/main/java/integrador/programa/repositorios/FacturaRepositorio.java
@@ -1,17 +1,19 @@
 package integrador.programa.repositorios;
 
 import integrador.programa.modelo.Factura;
+import integrador.programa.modelo.enumeradores.EstadoFactura;
+import integrador.programa.modelo.enumeradores.TipoComprobante;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface FacturaRepositorio extends JpaRepository<Factura, String> {
-    
-
-    //List<Factura> buscarPorEstado(EstadoFactura estado);
-
-    //List<Factura> buscarPorTipo(TipoComprobante tipo);
-    
-    //Optional<Factura> buscarPorNroSerie(String nroSerie);
+    List<Factura> findByEstado(EstadoFactura estado);
+    List<Factura> findByTipo(TipoComprobante tipo);
+    Optional<Factura> findByNroSerie(String nroSerie);
 }
