e51be10360f126d2c45b19c4fd8cf1386d9db46c
Nathelin <menachonatalian@gmail.com>
2025-11-16 10:42:43 -0300

    Implementación de alta ante anulación de factura.  Relacionado a #20

--- src/main/java/integrador/programa/servicios/NotaServicio.java
+++ src/main/java/integrador/programa/servicios/NotaServicio.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import integrador.programa.repositorios.NotaRepositorio;
+import integrador.programa.modelo.NotaCredito;
+import integrador.programa.modelo.Factura;
+import java.time.LocalDate;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class NotaServicio {
@@ -12,5 +16,17 @@ public class NotaServicio {
 
     public NotaServicio(NotaRepositorio notaRepositorio) {
         this.notaRepositorio = notaRepositorio;
+    }
+
+    @Transactional
+    public NotaCredito altaNotaPorFactura(Factura factura) {
+        if (factura == null) return null;
+        NotaCredito nota = new NotaCredito();
+        nota.setNroSerie(factura.getNroSerie());
+        nota.setPrecioTotal(factura.getPrecioTotal());
+        nota.setTipo(factura.getTipo());
+        nota.setFecha(LocalDate.now());
+        nota.setFacturaAnulada(factura);
+        return notaRepositorio.save(nota);
     }
 }
