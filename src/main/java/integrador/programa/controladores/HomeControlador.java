efd16e00eb5edd1e299a282680ef9d368358d0e1
gxickx <mgrokoe@gmail.com>
2025-11-16 16:05:47 -0300

    Modifiqué HomeControlador para que reciba una lista de servicios y clientes, ya que había un error y no dejaba entrar en las páginas. Unifiqué los scripts de JS en un único archivo main.js y borre las secciones de <script> de los HTML. Para mas orden

--- src/main/java/integrador/programa/controladores/HomeControlador.java
+++ src/main/java/integrador/programa/controladores/HomeControlador.java
@@ -1,14 +1,25 @@
 package integrador.programa.controladores;
-
+import integrador.programa.modelo.Cliente;
+import integrador.programa.modelo.Servicio;
+import integrador.programa.servicios.ClienteServicio;
+import integrador.programa.servicios.ServicioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeControlador extends Object {
 
-    public HomeControlador() {
+    // instancias de servicio
 
-    }
+    @Autowired
+    private ClienteServicio clienteServicio;
+
+    @Autowired
+    private ServicioServicio servicioServicio;
 
     @GetMapping("/")
     public String mostrarPaginaInicio(){
@@ -16,12 +27,20 @@ public class HomeControlador extends Object {
     }
 
     @GetMapping("/clientes")
-    public String irAClientes() {
+    public String irAClientes(Model model) {
+        List<Cliente> misClientes = clienteServicio.listarTodos();
+
+        model.addAttribute("clientes", misClientes);
         return "gestion-clientes-inicio";
     }
 
     @GetMapping("/servicios")
-    public String irAServicios() {
+    public String irAServicios(Model model) {
+
+        List<Servicio> misServicios = servicioServicio.listarTodos();
+
+        model.addAttribute("servicios", misServicios);
+
         return "gestion-servicio-abm";
     }
 
