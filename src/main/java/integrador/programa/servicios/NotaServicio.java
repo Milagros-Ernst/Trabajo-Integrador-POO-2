02cf82397872372d9270c52bf724fb2b881c15f2
Nathelin <menachonatalian@gmail.com>
2025-11-16 12:43:32 -0300

    Modificación de la creación de nota #20

--- src/main/java/integrador/programa/servicios/NotaServicio.java
+++ src/main/java/integrador/programa/servicios/NotaServicio.java
@@ -3,30 +3,44 @@ package integrador.programa.servicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import integrador.programa.repositorios.DetalleNotaRepositorio;
 import integrador.programa.repositorios.NotaRepositorio;
 import integrador.programa.modelo.NotaCredito;
+import integrador.programa.modelo.DetalleFactura;
+import integrador.programa.modelo.DetalleNota;
 import integrador.programa.modelo.Factura;
 import java.time.LocalDate;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class NotaServicio {
+    private static final String RESPONSABLE = "Admin Hardcodeado";
+
     @Autowired
     private final NotaRepositorio notaRepositorio;
+
 
     public NotaServicio(NotaRepositorio notaRepositorio) {
         this.notaRepositorio = notaRepositorio;
     }
 
-    @Transactional
-    public NotaCredito altaNotaPorFactura(Factura factura) {
-        if (factura == null) return null;
+    public NotaCredito altaNotaPorFactura(Factura facturaAnulada, String motivoAnulacion) {
         NotaCredito nota = new NotaCredito();
-        nota.setNroSerie(factura.getNroSerie());
-        nota.setPrecioTotal(factura.getPrecioTotal());
-        nota.setTipo(factura.getTipo());
         nota.setFecha(LocalDate.now());
-        nota.setFacturaAnulada(factura);
+        nota.setEmpleadoResponsable(RESPONSABLE);
+        nota.setMotivoAnulacion(motivoAnulacion); 
+        
+        nota.setPrecioTotal(facturaAnulada.getPrecioTotal() * -1); 
+        nota.setFacturaAnulada(facturaAnulada); 
+        
+        for (DetalleFactura detFactura : facturaAnulada.getDetalles()) {
+            DetalleNota detNota = new DetalleNota();
+            detNota.setDescripcion(detFactura.getDescripcion());
+            detNota.setPrecio(detFactura.getPrecio() * -1); 
+            detNota.setNotaCredito(nota);
+            
+            nota.getDetallesNota().add(detNota); 
+        }
+        
         return notaRepositorio.save(nota);
     }
 }
