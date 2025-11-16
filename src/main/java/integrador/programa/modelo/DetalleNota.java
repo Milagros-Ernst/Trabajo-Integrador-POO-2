14ddc7ace23dd39d007c59f9704f221cef547e19
Nathelin <menachonatalian@gmail.com>
2025-11-16 10:37:45 -0300

    Definición de la clase y relación con NotaCredito y DetalleFactura. Relacionado con #20

--- src/main/java/integrador/programa/modelo/DetalleNota.java
+++ src/main/java/integrador/programa/modelo/DetalleNota.java
@@ -1,15 +1,49 @@
 package integrador.programa.modelo;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "DetalleNota")
+@EqualsAndHashCode(exclude = {"detalleFactura", "notaCredito"})
+@ToString(exclude = {"detalleFactura", "notaCredito"})
 public class DetalleNota {
 
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id_detalle_nota", columnDefinition = "VARCHAR(36)")
+    @Setter(AccessLevel.NONE)
+    private String idDetalleNota;
 
-    // para cuando hayan servicios
-    /*public void agregarServicio(Servicio detalle) {
-        if (detalle != null) {
-            // detalle.setFactura(this);
-            if (!this.detalles.contains(detalle)) {
-                this.detalles.add(detalle);
-            }
-        }
-    }*/
+    @NotNull(message = "La descripción no puede ser nula")
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    @NotNull(message = "El precio no puede ser nulo")
+    @Positive(message = "El precio debe ser positivo")
+    @Column(name = "precio", nullable = false)
+    private double precio;
+
+    // Relación con DetalleFactura 
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_detalle_factura", referencedColumnName = "idDetalle", nullable = false, unique = true)
+    private DetalleFactura detalleFactura;
+
+    // Relación con NotaCredito 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_nota_credito", nullable = false)
+    @NotNull(message = "El detalle de nota debe pertenecer a una nota de crédito")
+    private NotaCredito notaCredito;
 }
+
